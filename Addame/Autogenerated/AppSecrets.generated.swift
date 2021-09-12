b4f4268aeb7c6c2fc393bb1c44ff04634cfc0773
saroar <saroar9@gmail.com>
2021-09-12 15:26:29 +0300

    remove auto generated files

--- Addame/Autogenerated/AppSecrets.generated.swift
+++
@@ -1,8 +0,0 @@
-// Generated using Sourcery 1.5.0 — https://github.com/krzysztofzablocki/Sourcery
-// DO NOT EDIT
-struct AppSecrets {
-  static let rootURL = ""
-  static let webSocketURL = ""
-  static let accessKeyId = ""
-  static let secretAccessKey = ""
-}
